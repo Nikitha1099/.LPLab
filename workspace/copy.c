92b7abc08e23f1943ccec00983cd53f88efcbdf3
SaiHemanthBR <sh.bheemreddy@icloud.com>
2020-07-17 11:07:07 +0530

    Updated doc

--- workspace/copy.c
+++ workspace/copy.c
@@ -25,7 +25,9 @@ int main(int argc, char *argv[]) {
     char buffer[BUF_SIZE];
 
     // Opening Source file. 
-    // int open(const char *pathname, int flags) return -1 if any error occurs
+    // int open(const char *pathname, int flags) 
+    // If successful, open() returns a non-negative integer, termed a file descriptor.  
+    // It returns -1 on failure
     int srcID = open(argv[1], O_RDONLY);
     if(srcID == -1) {
         printf("Unable to open source.txt\n");
@@ -33,7 +35,9 @@ int main(int argc, char *argv[]) {
     }
 
     // Opening Destination file
-    // int open(const char *pathname, int flags) return -1 if any error occurs
+    // int open(const char *pathname, int flags) 
+    // If successful, open() returns a non-negative integer, termed a file descriptor.  
+    // It returns -1 on failure
     int destID = open(argv[2], O_WRONLY | O_CREAT, 0644);
     if(destID == -1) {
         printf("Unable to open destination.txt\n");
